7e0abfdc165daaa8b8f2a430e0401a8bd9d8687b
darrancebeh <darrancebeh@gmail.com>
2022-12-15 08:24:09 +0800

    Fixed Practice 9

--- lab-6/lab6-09.py
+++ lab-6/lab6-09.py
@@ -2,9 +2,13 @@ def isPrime(n):
     a = 2;
     while(a < n-1):
         if(n % a == 0):
-            return (f"{n} is NOT a prime number.");
-    return (f"{n} is a prime number.");
+            return False;
+        a += 1;
+    return True;
 
 n = int(input("Enter N: "));
 
-print(isPrime(n));
+if(isPrime(n)):
+    print(f"{n} is a prime number.")
+else:
+    print(f"{n} is NOT a prime number.")
